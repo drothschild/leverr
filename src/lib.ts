0156ccba457cd6ccc06ca5ea92f3a9603a7a6d4b
David Rothschild <drothschild@gmail.com>
2026-02-24 01:12:21 -0800

    feat: add library exports for embedding as rule engine

---
+++ src/lib.ts
@@ -0,0 +1,7 @@
+// Library API exports for embedding Rill as a rule engine
+export { evaluate } from './evaluator';
+export { createPrelude } from './prelude';
+export { lex } from './lexer';
+export { parse } from './parser';
+export { Value, prettyPrint } from './values';
+export { runSource } from './runner';
